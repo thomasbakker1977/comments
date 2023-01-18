17d0982ab6fe27a1b7a0d9241053b4168e66a851
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-18 20:58:49 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+Exercise
+
+1
+Say yes more. Yes opens doors. No closes them. Yes pushes us. No keeps us safe at home. Imagine all the opportunities waiting for a yes.
+
+2
+Let go of expectation. You’ve done your best to prepare. Now, let go. There’s no one way your life should unfold. Enjoy the journey.
+
+3
+Welcome diversions. The most rewarding adventures often start with an unexpected detour. Perhaps that distraction will guide you onward.
---
+++ main.py
@@ -0,0 +1,35 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line 
+
+# Say yes more. Yes opens doors. No closes them.
+# Yes pushes us. # No keeps us safe at home.
+# Imagine all the opportunities waiting for a yes.
+
+# Let go of expectation.You've done your best to prepare.
+# Now, let go.
+# There's no one way your life should unfold. Enjoy the journey.
+
+# Welcome diversions.
+# The most rewarding adventures ofter start with an unexpected detour.
+# Perhaps that distraction will guide you onward."""
+
+"""
+Say yes more. Yes opens doors. No closes them.
+Yes pushes us. # No keeps us safe at home.
+Imagine all the opportunities waiting for a yes.
+"""
+
+"""
+Let go of expectation.You've done your best to prepare.
+Now, let go.
+There's no one way your life should unfold. # Enjoy the journey.
+"""
+
+"""
+Welcome diversions.
+The most rewarding adventures ofter start with an unexpected detour.
+Perhaps that distraction will guide you onward.
+"""
